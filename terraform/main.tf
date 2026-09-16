ab1d0934697aabd179dd80384e9f13cc61729ad5
Shipra <shipra.shrivastwa@gmail.com>
2026-09-16 15:12:07 +1000

    Add Terraform Azure infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "main" {
+  name                       = var.storage_account_name
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  account_tier               = "Standard"
+  account_replication_type   = "LRS"
+  min_tls_version            = "TLS1_2"
+  https_traffic_only_enabled = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "australiaeast"
+}
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+  default     = "rg-azure-devops-portfolio"
+}
+
+variable "storage_account_name" {
+  description = "Globally unique name for the Azure Storage Account"
+  type        = string
+}
