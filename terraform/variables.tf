c247e923632bca239950af734db095734bbfaf49
Shipra <shipra.shrivastwa@gmail.com>
2026-09-16 15:29:39 +1000

    added storage account name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,4 +13,5 @@ variable "resource_group_name" {
 variable "storage_account_name" {
   description = "Globally unique name for the Azure Storage Account"
   type        = string
+  default     = "stazgithubdeploy"
 }
